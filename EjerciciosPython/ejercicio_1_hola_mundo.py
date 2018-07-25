f3af64bd32ff54fc23fe506b1db025d93c40e893
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-07-24 23:01:33 -0500

    Ejercicio uno (hello world) acompletado. *added comment*

--- EjerciciosPython/ejercicio_1_hola_mundo.py
+++ EjerciciosPython/ejercicio_1_hola_mundo.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
 
-//https://www.hackerrank.com/challenges/py-hello-world/problem
+#https://www.hackerrank.com/challenges/py-hello-world/problem
     my_string = "Hello, World!"
     print (my_string)
