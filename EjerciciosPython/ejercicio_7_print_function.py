dfdae4bf48a9241d39af5a70d27070d9cb77b02c
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-07-25 00:44:28 -0500

    Ejercicio siete (print function) completado.

---
+++ EjerciciosPython/ejercicio_7_print_function.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+
+#https://www.hackerrank.com/challenges/python-print/problem?h_r=next-challenge&h_v=zen
+if __name__ == '__main__':
+    N = int(raw_input())
+
+    for i in range(N):
+        print (i+1,end="")
