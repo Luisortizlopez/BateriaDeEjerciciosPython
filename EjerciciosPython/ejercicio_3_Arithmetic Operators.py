69a2859bcb56ddf6ae33b212470c28f8ab8075f9
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-07-24 23:27:30 -0500

    Ejercicio tres (arithmetic-operators) completado.

---
+++ EjerciciosPython/ejercicio_3_Arithmetic Operators.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+
+    #https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+    a = int(raw_input())
+    b = int(raw_input())
+
+    sum = a + b
+    print (sum)
+    sub = a - b
+    print (sub)
+    times = a * b
+    print (times)
