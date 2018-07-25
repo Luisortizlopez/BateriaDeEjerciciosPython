9d8f2854da83abff1969b3a3d22337d1c8156375
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-07-24 23:40:02 -0500

    Ejercicio cuatro (python division) completado.

---
+++ EjerciciosPython/ejercicio_4_python_division.py
@@ -0,0 +1,12 @@
+from __future__ import division
+
+if __name__ == '__main__':
+
+    #https://www.hackerrank.com/challenges/python-division/problem?h_r=next-challenge&h_v=zen
+    a = int(raw_input())
+    b = int(raw_input())
+
+    division = a // b
+    print(division)
+    float_division = a / b
+    print(float_division)
