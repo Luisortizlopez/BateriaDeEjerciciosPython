9f54a1bd305695b23ac1bf36ceb5b824d37e4b41
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-07-29 18:48:56 -0500

    Ejercicio 14 (swap case) completado.

---
+++ EjerciciosPython/Ejercicio_14_swap_case.py
@@ -0,0 +1,4 @@
+#https://www.hackerrank.com/challenges/swap-case/problem
+def swap_case(s):
+    #This method returns a copy of the string in which all the case-based characters have had         their case swapped.
+    return s.swapcase()
