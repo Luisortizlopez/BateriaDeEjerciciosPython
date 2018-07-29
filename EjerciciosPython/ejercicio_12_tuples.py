05330a2f8d7a5dfff5f73af1ddca75d3b29edfea
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-07-29 17:25:48 -0500

    Ejercicio 12 (tuples) completado.

---
+++ EjerciciosPython/ejercicio_12_tuples.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(raw_input())
+    #Convert list elements from strings to ints
+    #Iterate list replacing strings to ints, split string into a list
+    integer_list = map(int, raw_input().split())
+
+    #Convert list of ints to a tuple of ints (tuples are hash able, list are not)
+    #Used to quickly compare dictionary keys during a dictionary lookup.
+    t = tuple(integer_list)
+
+    #Print the result of hash(t)
+    print hash(t)
