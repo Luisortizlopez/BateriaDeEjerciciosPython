eb44613f47173176dd388043a54fe737b62ed35b
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-07-29 19:10:11 -0500

    Ejercicio 15 (string split and join) completado.

---
+++ EjerciciosPython/Ejercicio_15_string_split_and_join.py
@@ -0,0 +1,5 @@
+#https://www.hackerrank.com/challenges/python-string-split-and-join/problem?h_r=next-challenge&h_v=zen
+def split_and_join(line):
+    # write your code here
+    #Return a string which is the concatenation of the strings
+    return "-".join(line.split())
