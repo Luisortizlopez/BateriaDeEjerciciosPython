533c16938c18360f16f2604c74ae7f712b1fbf47
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-07-29 20:18:56 -0500

    Ejercicio 16 (what's your name?) completado.

---
+++ EjerciciosPython/Ejercicio_16_What's_your_name.py
@@ -0,0 +1,4 @@
+#https://www.hackerrank.com/challenges/whats-your-name/problem
+def print_full_name(a, b):
+    #most basic usage is to insert values into a string with the %s placeholder.
+    print (("Hello %s %s! You just delved into python.") % (a, b))
